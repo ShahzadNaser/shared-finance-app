5a7f0e58c11fc5879eea9005093c04eccb33049e
Basma Maher <basmamaher@Basmas-MacBook-Pro.local>
2022-10-11 12:32:28 +0300

    custom field employee

---
+++ shared_finance_app/shared_finance_app/doctype/camp_location/camp_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, mesa_safd@hotmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Camp Location', {
+	// refresh: function(frm) {
+
+	// }
+});
