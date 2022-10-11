67098d170c6f08eec16ceaaecd49ea27406942cf
Basma Maher <basmamaher@Basmas-MacBook-Pro.local>
2022-10-11 11:53:12 +0300

    custom field employee

---
+++ shared_finance_app/shared_finance_app/doctype/sector/sector.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, mesa_safd@hotmail.com and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Sector', {
+	// refresh: function(frm) {
+
+	// }
+});
