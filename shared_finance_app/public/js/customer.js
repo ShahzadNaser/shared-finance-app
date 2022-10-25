1c2fec80f8de8eaf01b280d095a869658176fdb4
loubnahasna <loubna.hasna@exa.com.sa>
2022-10-25 09:38:37 +0300

    JLO-100

--- shared_finance_app/public/js/customer.js
+++ shared_finance_app/public/js/customer.js
@@ -9,11 +9,19 @@ frappe.ui.form.on('Customer', {
             frm.set_value('enable_eosb_formula_for_5_years_case_basic_salary_',1);
             frm.set_value('after_year',100);
 
+           }
+           else{
+               frm.set_value('enable_v_pay_formula_for_5_years_case_basic_salary_',0);
+            frm.set_value('after_years',null);
+            frm.set_value('enable_eosb_formula_for_5_years_case_basic_salary_',0);
+            frm.set_value('after_year',null);
+
+
+           }
             frm.refres_field("enable_v_pay_formula_for_5_years_case_basic_salary_");
             frm.refres_field("after_years");
             frm.refres_field("enable_eosb_formula_for_5_years_case_basic_salary_");
             frm.refres_field("after_year");
-           }
     },
 
 })
