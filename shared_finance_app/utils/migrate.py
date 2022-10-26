bcde823b240882b985f1e05e577b47aadfae3e5c
loubnahasna <loubna.hasna@exa.com.sa>
2022-10-26 14:34:15 +0300

    JES-905

--- shared_finance_app/utils/migrate.py
+++ shared_finance_app/utils/migrate.py
@@ -2,25 +2,40 @@ import frappe
 
 def after_migrate():
 
+    try:
 
-    frappe.db.sql(""" delete from `tabProperty Setter` where name='Payment Request-currencyccc-read_only';
-                     INSERT INTO `tabProperty Setter`
-                    (`default_value`,`doc_type`,`docstatus`,`doctype_or_field`,`field_name`,`modified`,`creation`,`name`,`property`,`property_type`,`value`)
-                    VALUES
-                    (0,'Payment Request',0,'DocField','currency','2022-10-16 14:38:31.442080','2022-10-16 14:38:31.442080','Payment Request-currencyccc-read_only','read_only','Check',0);
-                    """)
+        new_prop = frappe.get_doc({
+            "default_value": "0",
+            "doc_type": "Payment Request",
+            "docstatus": 0,
+            "doctype": "Property Setter",
+            "doctype_or_field": "DocField",
+            "field_name": "currency",
+            "modified": "2022-10-16 14:38:31.442080",
+            "name": "Payment Request-currency-read_only",
+            "property": "read_only",
+            "property_type": "Check",
+            "value": "0"
 
-    frappe.db.sql(""" delete from `tabProperty Setter` where name='Journal Entry-read_only';
-                        INSERT INTO `tabProperty Setter`
-                       (`default_value`,`doc_type`,`docstatus`,`doctype_or_field`,`field_name`,`modified`,`creation`,`name`,`property`,`property_type`,`value`)
-                       VALUES
-                       (0,'Journal Entry',0,'DocField','finance_book','2022-10-16 14:38:31.442080','2022-10-16 14:38:31.442080','Journal Entry-read_only','read_only','Check',0);
-                       """)
+        })
+        new_prop.insert(ignore_permissions=True, ignore_mandatory=True, ignore_if_duplicate=True)
 
+        new_prop = frappe.get_doc({
+            "default_value": "0",
+            "doc_type": "Journal Entry",
+            "docstatus": 0,
+            "doctype": "Property Setter",
+            "doctype_or_field": "DocField",
+            "field_name": "finance_book",
+            "modified": "2022-10-16 14:38:31.442080",
+            "name": "Payment Request-currency-read_only",
+            "property": "read_only",
+            "property_type": "Check",
+            "value": "0"
 
+        })
+        new_prop.insert(ignore_permissions=True, ignore_mandatory=True, ignore_if_duplicate=True)
 
-
-    frappe.db.commit()
-
-
-
+        frappe.db.commit()
+    except:
+        pass
