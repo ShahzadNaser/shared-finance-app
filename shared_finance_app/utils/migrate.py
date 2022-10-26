3bdac9df8841b3d889351260457c480bd278a63f
loubnahasna <loubna.hasna@exa.com.sa>
2022-10-26 13:56:56 +0300

    revert

--- shared_finance_app/utils/migrate.py
+++ shared_finance_app/utils/migrate.py
@@ -10,12 +10,6 @@ def after_migrate():
                     (0,'Payment Request',0,'DocField','currency','2022-10-16 14:38:31.442080','2022-10-16 14:38:31.442080','Payment Request-currencyccc-read_only','read_only','Check',0);
                     """)
 
-    frappe.db.sql(""" delete from `tabProperty Setter` where name='Payment Request-currencyccc-read_only';
-                         INSERT INTO `tabProperty Setter`
-                        (`default_value`,`doc_type`,`docstatus`,`doctype_or_field`,`field_name`,`modified`,`creation`,`name`,`property`,`property_type`,`value`)
-                        VALUES
-                        (0,'Payment Request',0,'DocField','currency','2022-10-16 14:38:31.442080','2022-10-16 14:38:31.442080','Payment Request-currencyccc-read_only','read_only','Check',0);
-                        """)
 
 
     frappe.db.commit()
