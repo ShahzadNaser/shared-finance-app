fc3a9bdd9b923131bd2023c884e08a08ba14a8e3
eng.loubna <eng.loubna.hasna@gmail.com>
2022-10-26 10:53:08 +0000

    Jlo 93

---
+++ shared_finance_app/utils/migrate.py
@@ -0,0 +1,16 @@
+import frappe
+
+def after_migrate():
+
+
+    frappe.db.sql(""" delete from `tabProperty Setter` where name='Payment Request-currencyccc-read_only';
+    INSERT INTO `tabProperty Setter`
+                    (`default_value`,`doc_type`,`docstatus`,`doctype_or_field`,`field_name`,`modified`,`creation`,`name`,`property`,`property_type`,`value`)
+                    VALUES
+                    (0,'Payment Request',0,'DocField','currency','2022-10-16 14:38:31.442080','2022-10-16 14:38:31.442080','Payment Request-currencyccc-read_only','read_only','Check',0);
+                    """)
+
+    frappe.db.commit()
+
+
+
