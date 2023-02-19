8fc314ffe70a458a724190b3d5900b1428657fa3
ubunto <you@example.com>
2023-02-19 10:41:41 +0300

    JLO-121

--- shared_finance_app/overrides_class/payment_request.py
+++ shared_finance_app/overrides_class/payment_request.py
@@ -26,7 +26,7 @@ class CustomPaymentRequest(PaymentRequest):
 		self.grand_total = 0
 		self.total_of_advance_paid = 0
 		self.total_now_being_requested = 0
-		if self.pay_to_party:
+		if self.pay_to_party==0:
 			for row in self.payment_request_item:
 				self.grand_total += row.amount or 0
 				self.total_of_advance_paid += row.less_advance_paid or 0
